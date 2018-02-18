a99b3c737db090558ef77e939e01d17fb967869e
sunshine <17765293970@163.com>
2018-02-18 22:13:32 +0800

    头部组件修改

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -2,29 +2,24 @@
     <div class="navbar">
         <div class="navbar-inner">
             <div class="container">
-                <a class="brand" href="/">
-                    <img src="//o4j806krb.qnssl.com/public/images/cnodejs_light.svg">
-                </a>
-                <form id="search_form" class="navbar-search" action="/search">
-                    <input type="text" id="q" name="q" class="search-query span3" value="">
-                </form>
+                <router-link class="brand" to="/home">
+                    <img src="//o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="logo" />
+                </router-link>
                 <ul class="nav pull-right">
                     <li><router-link to="/home">首页</router-link></li>
-                    <li><router-link to="/my/messages">未读消息</router-link>
-                    </li>
+                    <li v-if="islogin"><router-link to="/messages"><span class="big messages_count">1</span>未读消息</router-link></li>
+                    <li v-if="islogin"><router-link to="/create/new">发布话题</router-link></li>
                     <li><router-link to="/getstart">新手入门</router-link></li>
                     <li><router-link to="/api">API</router-link></li>
-                    <li><router-link to="/about" target="">关于</router-link></li>
-                    <li><router-link to="/setting">设置</router-link></li>
-                    <li>
-                        <a href="/signout" data-method="post" rel="nofollow">退出</a>
-                    </li>
+                    <li><router-link to="/about">关于</router-link></li>
+                    <li v-if="islogin"><a>设置</a></li>
+                    <li v-if="islogin"><a>退出</a></li>
                 </ul>
                 <a class="btn btn-navbar" id="responsive-sidebar-trigger">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
             </div>
         </div>
     </div>
@@ -36,5 +31,6 @@
 
     @Component({})
     export default class Header extends Vue {
+        islogin: boolean = false
     };
 </script>
