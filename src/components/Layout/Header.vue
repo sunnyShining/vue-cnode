0434a7db35ab281d48956e8800e7c96d6031d875
sunshine <17765293970@163.com>
2018-02-18 22:03:38 +0800

    新增几个静态页面

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -9,13 +9,13 @@
                     <input type="text" id="q" name="q" class="search-query span3" value="">
                 </form>
                 <ul class="nav pull-right">
-                    <li><a href="/">首页</a></li>
-                    <li><a href="/my/messages">未读消息</a>
+                    <li><router-link to="/home">首页</router-link></li>
+                    <li><router-link to="/my/messages">未读消息</router-link>
                     </li>
-                    <li><a href="/getstart">新手入门</a></li>
-                    <li><a href="/api">API</a></li>
-                    <li><a href="/about" target="">关于</a></li>
-                    <li><a href="/setting">设置</a></li>
+                    <li><router-link to="/getstart">新手入门</router-link></li>
+                    <li><router-link to="/api">API</router-link></li>
+                    <li><router-link to="/about" target="">关于</router-link></li>
+                    <li><router-link to="/setting">设置</router-link></li>
                     <li>
                         <a href="/signout" data-method="post" rel="nofollow">退出</a>
                     </li>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/home/home.vue';
 import Getstart from '@/pages/getstart/getstart.vue';
+import About from '@/pages/about/about.vue';
+import Api from '@/pages/api/api.vue';
 
 Vue.use(Router);
 
@@ -16,6 +18,16 @@ export default new Router({
 	      	path: '/getstart',
 	      	name: 'getstart',
 	      	component: Getstart,
+	    },
+	    {
+	      	path: '/about',
+	      	name: 'about',
+	      	component: About,
+	    },
+	    {
+	      	path: '/api',
+	      	name: 'api',
+	      	component: Api,
 	    },
 	    {
         	path: '*',
