42029d5a1429a3c49d1d4b155dcf9c1a9f753875
sunshine <17765293970@163.com>
2018-03-01 19:30:18 +0800

    路由懒加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,26 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/pages/home/home.vue';
-import Detail from '@/pages/detail/detail.vue';
-import User from '@/pages/user/user.vue';
-import Getstart from '@/pages/getstart/getstart.vue';
-import About from '@/pages/about/about.vue';
-import Api from '@/pages/api/api.vue';
-import Create from '@/pages/create/create.vue';
-import Messages from '@/pages/messages/messages.vue';
-import UserTopic from '@/pages/userTopic/userTopic.vue';
-import NoFound from '@/pages/noFound/noFound.vue';
+// import Home from '@/pages/home/home.vue';
+// import Detail from '@/pages/detail/detail.vue';
+// import User from '@/pages/user/user.vue';
+// import Getstart from '@/pages/getstart/getstart.vue';
+// import About from '@/pages/about/about.vue';
+// import Api from '@/pages/api/api.vue';
+// import Create from '@/pages/create/create.vue';
+// import Messages from '@/pages/messages/messages.vue';
+// import UserTopic from '@/pages/userTopic/userTopic.vue';
+// import NoFound from '@/pages/noFound/noFound.vue';
+
+const Home = (r: any) => require.ensure([], () => r(require('@/pages/home/home.vue')));
+const Detail = (r: any) => require.ensure([], () => r(require('@/pages/detail/detail.vue')));
+const User = (r: any) => require.ensure([], () => r(require('@/pages/user/user.vue')));
+const Getstart = (r: any) => require.ensure([], () => r(require('@/pages/getstart/getstart.vue')));
+const About = (r: any) => require.ensure([], () => r(require('@/pages/about/about.vue')));
+const Api = (r: any) => require.ensure([], () => r(require('@/pages/api/api.vue')));
+const Create = (r: any) => require.ensure([], () => r(require('@/pages/create/create.vue')));
+const Messages = (r: any) => require.ensure([], () => r(require('@/pages/messages/messages.vue')));
+const UserTopic = (r: any) => require.ensure([], () => r(require('@/pages/userTopic/userTopic.vue')));
+const NoFound = (r: any) => require.ensure([], () => r(require('@/pages/noFound/noFound.vue')));
 
 Vue.use(Router);
 
--- src/vue-shim.d.ts
+++ src/vue-shim.d.ts
@@ -4,3 +4,5 @@ declare module "*.vue" {
 }
 
 declare module 'mavon-editor';
+
+declare var require: RequireFunction;
